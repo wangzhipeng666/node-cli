08d001b3678343959be6e7ff66b4d76fd8cd035d
汪智鹏 <1848993253@qq.com>
2023-07-04 11:03:13 +0800

    命令行问答交互

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // 可选择的框架
+  framwork: ["express", "koa", "egg"],
+  // 框架对应的下载地址
+  foramworkUrl: {
+    express: "git@gitee.com:beiyaoyaoyao/express-template.git",
+    koa: "git@gitee.com:beiyaoyaoyao/koa-template.git",
+    egg: "git@gitee.com:beiyaoyaoyao/egg-template.git",
+  },
+};
